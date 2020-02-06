62ec69eecb5fed7e09e13ac63d5a456b3d3a4923
Dominique Nguyen <domnguyen@gmail.com>
2020-02-06 13:54:32 -0800

    learning the basics of gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/header"
+export default () => (
+    <div style={{ color: `teal` }}>
+        <Header headerText = "About Gatsby" />
+        <Header headerText = "Hello, I'm reusable" />
+        <p>Such wow. Very React.</p>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div>
+        <div> Hello world!</div >
+        <Link to='/contact/'>Contact</Link>
+        <Header headerText='Hi, Im Dom' />
+    </div>
+)
+
+
