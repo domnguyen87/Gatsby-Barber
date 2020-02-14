1b5efe5a4c0b2964f281d152b19fbc554f4a0e38
Dominique Nguyen <domnguyen@gmail.com>
2020-02-13 21:27:05 -0800

    add more scripts and preloader

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,25 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+// import { Link } from "gatsby"
+// import Header from "../components/header"
 
 export default () => (
     <div>
-        <div> Hello world!</div >
+        {/* <div> Hello world!</div >
         <Link to='/contact/'>Contact</Link>
-        <Header headerText='Hi, Im Dom' />
+        <Header headerText='Hi, Im Dom' /> */}
+        <div class="preloader">
+            <div class="spinner">
+                <div class="bounce1"></div>
+                <div class="bounce2"></div>
+                <div class="bounce3"></div>
+            </div>
+
+
+        </div>
+        {/* Footer scripts */}
+        <script src="http://demo.zytheme.com/hairy/assets/js/jquery-2.2.4.min.js"></script>
+        <script src="http://demo.zytheme.com/hairy/assets/js/plugins.js"></script>
+        <script src="http://demo.zytheme.com/hairy/assets/js/functions.js"></script>
     </div>
 )
 
