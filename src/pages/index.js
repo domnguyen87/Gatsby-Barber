813c33fe5333c5d69859a5e0b061fae134f13752
Dominique Nguyen <domnguyen@gmail.com>
2020-02-20 23:16:56 -0800

    add pricing #2

--- src/pages/index.js
+++ src/pages/index.js
@@ -543,6 +543,131 @@ export default () => (
                 {/* <!-- .container end --> */}
             </section>
             {/* <!-- #service1 end --> */}
+
+            {/* <!-- pricing #2============================================= --> */}
+            <section id="pricing" class="pricing pricing-2 bg-white pb-80">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
+                            <div class="heading text--center mb-70">
+                                <h2 class="heading--title">Our Pricing</h2>
+                                <p class="heading--desc">Duis aute irure dolor in reprehenderit volupte velit esse cillum dolore eu fugiat pariatursint occaecat cupidatat non proident culpa.</p>
+                                <div class="divider--line"></div>
+                            </div>
+                        </div>
+                        {/* <!-- .col-md-6 end --> */}
+                    </div>
+                    {/* <!-- .row end --> */}
+                    <div class="row">
+                        {/* <!-- Pricing #1 --> */}
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                            <div class="pricing-panel">
+                                <div class="pricing--content">
+                                    <h4 class="pricing--heading">Haircut</h4>
+                                    <div class="pricing--divider"></div>
+                                    <span class="price">$20.00</span>
+                                </div>
+                                <p class="pricing--desc">Our stylist consults & delivers you a precision haircut.</p>
+                            </div>
+                            {/* <!-- .panel end --> */}
+                        </div>
+                        {/* <!-- .col-md-4 end --> */}
+                        {/* <!-- Pricing #2 --> */}
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                            <div class="pricing-panel">
+                                <div class="pricing--content">
+                                    <h4 class="pricing--heading">Moustache Trim</h4>
+                                    <div class="pricing--divider"></div>
+                                    <span class="price">$10.00</span>
+                                </div>
+                                <p class="pricing--desc">Select & Change your hair color for new experience.</p>
+                            </div>
+                            {/* <!-- .panel end --> */}
+                        </div>
+                        {/* <!-- .col-md-4 end --> */}
+                        {/* <!-- Pricing #3 --> */}
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                            <div class="pricing-panel">
+                                <div class="pricing--content">
+                                    <h4 class="pricing--heading">Beard Trim</h4>
+                                    <div class="pricing--divider"></div>
+                                    <span class="price">$15.00</span>
+                                </div>
+                                <p class="pricing--desc">Keep your beard clean and sharp with an awesome style.</p>
+                            </div>
+                            {/* <!-- .panel end --> */}
+                        </div>
+                        {/* <!-- .col-md-4 end --> */}
+                        {/* <!-- Pricing #4 --> */}
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                            <div class="pricing-panel">
+                                <div class="pricing--content">
+                                    <h4 class="pricing--heading">Hair Wash</h4>
+                                    <div class="pricing--divider"></div>
+                                    <span class="price">$6.00</span>
+                                </div>
+                                <p class="pricing--desc">Relax and have a hot towel for cleaning your face.</p>
+                            </div>
+                            {/* <!-- .panel end --> */}
+                        </div>
+                        {/* <!-- .col-md-4 end --> */}
+                        {/* <!-- Pricing #5 --> */}
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                            <div class="pricing-panel">
+                                <div class="pricing--content">
+                                    <h4 class="pricing--heading">Hair Color</h4>
+                                    <div class="pricing--divider"></div>
+                                    <span class="price">$18.00</span>
+                                </div>
+                                <p class="pricing--desc">Select & Change your hair color for new experience.</p>
+                            </div>
+                            {/* <!-- .panel end --> */}
+                        </div>
+                        {/* <!-- .col-md-4 end --> */}
+                        {/* <!-- Pricing #6 --> */}
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                            <div class="pricing-panel">
+                                <div class="pricing--content">
+                                    <h4 class="pricing--heading">Face Mask</h4>
+                                    <div class="pricing--divider"></div>
+                                    <span class="price">$12.00</span>
+                                </div>
+                                <p class="pricing--desc">Our stylist consults & delivers you a precision haircut.</p>
+                            </div>
+                            {/* <!-- .panel end --> */}
+                        </div>
+                        {/* <!-- .col-md-4 end --> */}
+                        {/* <!-- Pricing #7 --> */}
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                            <div class="pricing-panel">
+                                <div class="pricing--content">
+                                    <h4 class="pricing--heading">Men’s Facial</h4>
+                                    <div class="pricing--divider"></div>
+                                    <span class="price">$25.00</span>
+                                </div>
+                                <p class="pricing--desc">Relax and have a hot towel for cleaning your face.</p>
+                            </div>
+                            {/* <!-- .panel end --> */}
+                        </div>
+                        {/* <!-- .col-md-4 end --> */}
+                        {/* <!-- Pricing #8 --> */}
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                            <div class="pricing-panel">
+                                <div class="pricing--content">
+                                    <h4 class="pricing--heading">Line Up</h4>
+                                    <div class="pricing--divider"></div>
+                                    <span class="price">$13.00</span>
+                                </div>
+                                <p class="pricing--desc">Keep your beard clean and sharp with an awesome style.</p>
+                            </div>
+                            {/* <!-- .panel end --> */}
+                        </div>
+                    </div>
+                    {/* <!-- .row end --> */}
+                </div>
+                {/* <!-- .container end --> */}
+            </section>
+            {/* <!-- #pricing1 end --> */}
         </div>
 
 
