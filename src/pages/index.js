ac17d72c09664e1d341efdecb832fe219e771ccc
Dominique Nguyen <domnguyen@gmail.com>
2020-03-03 22:22:53 -0800

    add footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -931,7 +931,129 @@ export default () => (
                 {/* <!-- .container end --> */}
             </section>
             {/* <!-- #clients1 end --> */}
+
+            {/* <!-- Footer #5============================================= --> */}
+            <footer id="footer" class="footer">
+                {/* <!-- Widget Section============================================= --> */}
+                <div class="footer-widget">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6 col-md-4 footer--widget-about">
+                                <div class="footer--widget-content">
+                                    <img class="mb-20" src="http://demo.zytheme.com/hairy/assets/images/logo/logo-light.png" alt="logo" />
+                                    <p>Proin gravida nibh vel velit auctor aliquet anean lorem quis. bindum auctor, nisi elite conset ipsums sagtis id duis sed odio sit.</p>
+                                    <div class="work--schedule clearfix">
+                                        <ul class="list-unstyled">
+                                            <li>Monday - Friday <span>9.00 : 17.00</span></li>
+                                            <li>Saturday <span>9.00 : 15.00</span></li>
+                                            <li>Sunday <span>9.00 : 13.00</span></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* <!-- .col-md-4 end --> */}
+                            <div class="col-xs-12 col-sm-6 col-md-4 footer--widget-recent">
+                                <div class="footer--widget-title">
+                                    <h5>Latest Posts</h5>
+                                </div>
+                                <div class="footer--widget-content">
+                                    <div class="entry">
+                                        <div class="entry--img">
+                                            <a href="#">
+                                                <img src="http://demo.zytheme.com/hairy/assets/images/blog/thumb/5.jpg" alt="entry" />
+                                            </a>
+                                        </div>
+                                        <div class="entry--content">
+                                            <div class="entry--title">
+                                                <a href="#">Essential barbering tips you need to know before you start</a>
+                                            </div>
+                                            <div class="entry--meta">
+                                                <span>Nov 09, 2017</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {/* <!-- .entry end --> */}
+
+                                    <div class="entry">
+                                        <div class="entry--img">
+                                            <a href="#">
+                                                <img src="http://demo.zytheme.com/hairy/assets/images/blog/thumb/4.jpg" alt="entry" />
+                                            </a>
+                                        </div>
+                                        <div class="entry--content">
+                                            <div class="entry--title">
+                                                <a href="#">What are the 360 waves? and how you can get them</a>
+                                            </div>
+                                            <div class="entry--meta">
+                                                <span>Oct 30, 2017</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {/* <!-- .entry end --> */}
+
+                                    <div class="entry">
+                                        <div class="entry--img">
+                                            <a href="#">
+                                                <img src="http://demo.zytheme.com/hairy/assets/images/blog/thumb/3.jpg" alt="entry" />
+                                            </a>
+                                        </div>
+                                        <div class="entry--content">
+                                            <div class="entry--title">
+                                                <a href="#">Discover what is the best haircut for your face shape?</a>
+                                            </div>
+                                            <div class="entry--meta">
+                                                <span>Oct 19, 2017</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {/* <!-- .entry end --> */}
+                                </div>
+                            </div>
+                            {/* <!-- .col-md-4 end --> */}
+                            <div class="col-xs-12 col-sm-6 col-md-4 footer--widget-contact">
+                                <div class="footer--widget-title">
+                                    <h5>Get In Touch</h5>
+                                </div>
+                                <div class="footer--widget-content">
+                                    <ul class="list-unstyled mb-0">
+                                        <li><i class="fa fa-map-marker"></i> 1220 Petersham town, Wardll St New South Wales Australia PA 6550.</li>
+                                        <li><i class="fa fa-phone"></i> (04) 491 570 110</li>
+                                        <li><i class="fa fa-envelope-o"></i> contact@zytheme.com</li>
+                                    </ul>
+                                    <form class="form--newsletter">
+                                        <input type="email" name="email" class="form-control" placeholder="Email address" />
+                                        <button type="submit">Subscribe</button>
+                                    </form>
+                                </div>
+                            </div>
+                            {/* <!-- .col-md-4 end --> */}
+                        </div>
+                    </div>
+                    {/* <!-- .container end --> */}
+                </div>
+                {/* <!-- .footer-widget end --> */}
+                {/* <!-- Copyrights============================================= --> */}
+                <div class="footer--copyright">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-6">
+                                <span>&copy; 2017, All rights reserved.</span>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-6 text-right">
+                                <div class="social">
+                                    <a class="share-twitter" href="#"><i class="fa fa-twitter"></i></a>
+                                    <a class="share-facebook" href="#"><i class="fa fa-facebook"></i></a>
+                                    <a class="share-linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+                                    <a class="share-pinterest" href="#"><i class="fa fa-instagram"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <!-- .container end --> */}
+                </div>
+            </footer>
         </div>
+        {/* </div>       <!-- #wrapper end --> */}
     </div>
 
 
